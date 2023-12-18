d24ca06631786715e5f5f9662c8cbb06629fccea
Leonardo Cardillo <124011140+leoncardi@users.noreply.github.com>
2023-12-18 08:39:54 +0000

    FEAT(resources.py): Add Table Scrape Handler initial code

--- pom_scraper_utils/resources.py
+++ pom_scraper_utils/resources.py
@@ -1,5 +1,57 @@
 from entrymaven import l
 
+class TableScrapeHandler:
+        def __init__(self, page_handler: 'PageHandlerObject'):
+            self.page = page_handler.page
+            self.page_id = page_handler.page_id
+            self.TABLE_SELECTOR = 'placeholder'
+            self.raw_data = []
+
+        async def multiple_target_screenshot(self, target, title):
+            try:
+                await target.scroll_into_view_if_needed()
+                await target.wait_for_timeout(2000)
+                await target.screenshot(path=f'database/raw_targets_screenshots/{title}.jpg')
+                l.info(f'(Page: {self.page_id}) Moved to table and screenshot of the target {title} captured successfully')
+            except Exception as e:
+                l.error(f'(Page: {self.page_id}) {e}')
+
+        async def multiple_target_finder(self, table_selector: str, screenshot: bool) -> list:
+            founded_targets = await self.page.query_selector_all(f"div [id^='{table_selector}'] table")
+            if founded_targets:
+                for i, target in enumerate(founded_targets, start=1):
+                    aria_label = await target.get_attribute('aria-label')
+                    l.info(f'{aria_label} (index: {i}): Aria-label found')
+                    
+                    if aria_label:
+                        screenshot_file_name = f"database/{aria_label.replace(' ', '_')}"
+                    else:
+                        screenshot_file_name = f'database/table_{i}'
+                    
+                    if screenshot:
+                        await self.multiple_target_screenshot(target, screenshot_file_name)     
+            else:
+                l.error(f'(Page: {self.page_id}) No targets found with the selector {table_selector}')
+                return None
+            return founded_targets
+
+        async def multiple_table_scraper(self, tables: list) -> list:
+            for table in tables:
+                headers = await table.query_selector_all('.footable-header')
+                datapoints = await table.query_selector_all('tr')
+
+                for header in headers:
+                    content = await header.inner_text()
+                    self.raw_data.append(f'Header: {content}')
+                
+                for datapoint in datapoints:
+                    data_variables = await datapoint.query_selector_all('td')
+                    for data_variable in data_variables:
+                        content = await data_variable.inner_text()
+                        self.raw_data.append(f'{content}')
+            print(self.raw_data)
+            return self.raw_data
+      
 class ChartScrapeHandler:
         def __init__(self, page_handler: 'PageHandlerObject'):
             self.page_handler = page_handler
@@ -26,9 +78,9 @@ class ChartScrapeHandler:
                 if screenshot == False:
                     await target.scroll_into_view_if_needed()
                     await self.page.wait_for_timeout(2000)  
-                    
                 else:
                     await self.multiple_target_screenshot(target, title)
+                    
             return self.founded_iframes_selectors
 
         async def multiple_barchart_scraper(self, iframes: list) -> list:
