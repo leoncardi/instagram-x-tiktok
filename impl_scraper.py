c2c6488a10f68e0bfcc0259215f0cfb8c3422b74
Leonardo Cardillo <124011140+leoncardi@users.noreply.github.com>
2023-12-20 01:40:45 +0000

    REFAC(impl_scraper.py): Base function addition and log file naming
    - Add a base function to manage the implementation structure
    - Introduce a function responsible for the implementation itself, which will use the base function as support to perform multiple asynchronous data scrapes
    - Change the name of the log file from 'ELT.log' to 'scraper.log' to establish a specialization, aiming to better organize possible future .log files

--- impl_scraper.py
+++ impl_scraper.py
@@ -1,23 +1,58 @@
 import asyncio
-from playwright.async_api import async_playwright
 
 import entrymaven as emav
-from pom_scraper_utils.browser import BrowserHandler as bh
+from playwright.async_api import async_playwright
+
+from pom_scraper_utils.browser import BrowserHandler
 from pom_scraper_utils.page import PageHandler
+from pom_scraper_utils.resources import ChartScrapeHandler
+from pom_scraper_utils.resources import TableScrapeHandler
+from db_handler import SQLiteHandler
 
 log_config = emav.Essentials()
-generic_logger = log_config.gen(filename='ELT.log')
-l = emav.l
+generic_logger = log_config.gen(filename='scraper.log')
+l = emav.l 
+
+async def base(
+    p: 'Playwright',
+    page_id: str,
+    target_url: str,
+    raw_dataset_name: str
+    ):
+
+    browser, context = await BrowserHandler.launch(p)
+    target_ph = PageHandler(page_id, context)
+    await target_ph.new_page()
+    await target_ph.goto(target_url)
+    
+    table_scraper = TableScrapeHandler(target_ph)
+    chart_scraper = ChartScrapeHandler(target_ph)
+
+    founded_tables = await table_scraper.multiple_target_finder(
+        table_selector='footable_parent_',
+        screenshot=False
+    )
+    
+    founded_charts = await chart_scraper.multiple_target_finder(
+        chart_selector='infogram-embed',
+        screenshot=True
+    )
+
+    table_data = await table_scraper.multiple_table_scraper(tables=founded_tables)
+    chart_data = await chart_scraper.multiple_barchart_scraper(iframes=founded_charts)  
+
+    sql_handler = SQLiteHandler('database', 'raw.db')
+    sql_handler.create_dataset_for_chart_data(
+        extracted_data=chart_data,
+        table_name=raw_dataset_name
+    )
 
 async def main():
     async with async_playwright() as p:
         try:
-            browser, context = await bh.launch(p)    
-            gram_ph = PageHandler('Instagram', context)
-            await gram_ph.new_page()     
-            await gram_ph.goto('https://www.businessofapps.com/data/instagram-statistics/')
-        except Exception as e:
-            l.error(f'(impl_scraper.main) {e}')
-            raise e
-        
-asyncio.run(main())
+            l.info('[NEW SCRAPING SESSION STARTED]')
+            #code
+        finally:
+            l.info('[CURRENT SCRAPING SESSION ENDED]')
+if __name__ == '__main__':
+    asyncio.run(main())
