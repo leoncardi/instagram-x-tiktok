c5e64364b347ee3cb41741063280f9921f359e8f
leonardo cardillo <leoncardi.contato@gmail.com>
2023-12-28 22:42:18 -0300

    FEAT(impl_scraper.py): Add end-to-end scraping script ready for use
    - Added impl_scraper.py
    - Added impl_scraper.py output results (raw.db, data screenshots, scraper.log)

--- impl_scraper.py
+++ impl_scraper.py
@@ -1,58 +1,73 @@
 import asyncio
 
-import entrymaven as emav
+from entrymaven import l, Essentials
 from playwright.async_api import async_playwright
 
-from pom_scraper_utils.browser import BrowserHandler
-from pom_scraper_utils.page import PageHandler
-from pom_scraper_utils.resources import ChartScrapeHandler
-from pom_scraper_utils.resources import TableScrapeHandler
-from db_handler import SQLiteHandler
+from database.adb_handler import AsyncSQLiteHandler
+from pom_p_scraper_utils import (
+    BrowserHandler,
+    PageHandler,
+    ChartScrapeHandler,
+    TableScrapeHandler)
 
-log_config = emav.Essentials()
-generic_logger = log_config.gen(filename='scraper.log')
-l = emav.l 
+Essentials.gen(filename='scraper.log')
+pages_id = ['Instagram', 'TikTok']
+targets_url = ['https://www.businessofapps.com/data/instagram-statistics/', 'https://www.businessofapps.com/data/tik-tok-statistics/']
+db_file_name = 'raw'
+raw_dataset_names = ['raw_gram', 'raw_tktk']
 
-async def base(
-    p: 'Playwright',
-    page_id: str,
-    target_url: str,
-    raw_dataset_name: str
-    ):
-
-    browser, context = await BrowserHandler.launch(p)
+async def scraper_base(context: 'PlaywrightContextOjbect',
+        page_id: str, target_url: str) -> (list, list):
     target_ph = PageHandler(page_id, context)
     await target_ph.new_page()
     await target_ph.goto(target_url)
+    await target_ph.popup_checkout()
     
     table_scraper = TableScrapeHandler(target_ph)
     chart_scraper = ChartScrapeHandler(target_ph)
 
-    founded_tables = await table_scraper.multiple_target_finder(
-        table_selector='footable_parent_',
-        screenshot=False
-    )
-    
-    founded_charts = await chart_scraper.multiple_target_finder(
-        chart_selector='infogram-embed',
-        screenshot=True
-    )
+    founded_tables = await table_scraper.multi_table_finder(table_selector='footable_parent_', screenshot=True) 
+    founded_charts = await chart_scraper.multi_chart_finder(chart_selector='infogram-embed', screenshot=True)
 
-    table_data = await table_scraper.multiple_table_scraper(tables=founded_tables)
-    chart_data = await chart_scraper.multiple_barchart_scraper(iframes=founded_charts)  
+    raw_table_data = await table_scraper.multi_table_scraper(tables=founded_tables)
+    raw_chart_data = await chart_scraper.multi_chart_scraper(charts=founded_charts)
 
-    sql_handler = SQLiteHandler('database', 'raw.db')
-    sql_handler.create_dataset_for_chart_data(
-        extracted_data=chart_data,
-        table_name=raw_dataset_name
-    )
+    await target_ph.close()
+    return raw_table_data, raw_chart_data
 
-async def main():
+async def single_loader_base(raw_dataset_name: str, data: list, 
+        data_type: str, sql_handler: AsyncSQLiteHandler):
+    for i, item in enumerate(data):
+        item_name = f'{raw_dataset_name}_{data_type}_{i}'
+        
+        await sql_handler.create_table(item_name)
+        await sql_handler.insert_extracted_raw_data([item], item_name)
+    l.info(f'(Database: {sql_handler.db_file_name}.db) All expected attempted {data_type}s from {raw_dataset_name} datapoints commit ended')
+
+async def multi_loader_base(sql_handler: AsyncSQLiteHandler, raw_dataset_name: str, 
+        tables_data: list, charts_data: list):
+    l.info(f'(Database: {db_file_name}.db) Attempting to commit all target data')
+
+    await single_loader_base(raw_dataset_name, tables_data, 'table', sql_handler)
+    await single_loader_base(raw_dataset_name, charts_data, 'chart', sql_handler)
+
+    l.info(f'(Database: {db_file_name}.db) All attempted expected target data commit ended') 
+
+async def scraper_main() -> list:
     async with async_playwright() as p:
-        try:
-            l.info('[NEW SCRAPING SESSION STARTED]')
-            #code
-        finally:
-            l.info('[CURRENT SCRAPING SESSION ENDED]')
+        browser_context = await BrowserHandler.launch_chromium(p)      
+        tasks = [scraper_base(browser_context, page_id, target_url) for page_id, target_url in zip(pages_id, targets_url)]
+        extracted_raw_data = await asyncio.gather(*tasks)
+        return extracted_raw_data
+
+async def loader_main(extracted_raw_data: list):
+    async with AsyncSQLiteHandler('database', db_file_name) as sql_handler:
+        tasks = [multi_loader_base(sql_handler, raw_dataset_name, table_data, chart_data) for raw_dataset_name, (table_data, chart_data) 
+            in zip(raw_dataset_names, extracted_raw_data)]
+        await asyncio.gather(*tasks)
+    
 if __name__ == '__main__':
-    asyncio.run(main())
+    l.info('[NEW SCRAPING SESSION STARTED]')
+    extracted_raw_data = asyncio.run(scraper_main())
+    asyncio.run(loader_main(extracted_raw_data))
+    l.info('[CURRENT SCRAPING SESSION ENDED]')   
