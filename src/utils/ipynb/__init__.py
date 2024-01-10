c30e70f2137d7426c37edaa91d60bedc1833cea5
Leonardo Cardillo <124011140+leoncardi@users.noreply.github.com>
2024-01-10 17:54:16 +0000

    REFAC(wrlg): Finalize all initial data wrlg code and make it functional
    Note: This module/code is subject to drastic changes in the future

--- src/utils/ipynb/__init__.py
+++
@@ -1,2 +0,0 @@
-from .visualizers import DataFrameVisualizer
-from .db_connector import get_raw_db_path
--- src/utils/ipynb/db_connector.py
+++ src/utils/ipynb/db_connector.py
@@ -1,3 +1,7 @@
 def get_raw_db_path(root_dir):
     """Assists sqlite connections by providing the absolute directory for 'raw.db'"""
     return f'{root_dir}/database/01_raw.db'
+
+def get_transf_db_path(root_dir):
+    """Assists sqlite connections by providing the absolute directory for 'transformed.db'"""
+    return f'{root_dir}/database/02_transformed.db'
